5d3b3a38fdd90e79bdf046e5c44f77f1ce36332e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-21 08:30:02 -0400

    Se agrego la seccion del grupo

---
+++ components/components_Home/Components/Section_Grupo.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import ImagenGrupo from "@/../public/img/ImagenGrupo.svg";
+
+export default function Section_Grupo() {
+  return (
+    <section className="py-10 flex  flex-col justify-center items-center bg-linear-to-tl from-blue-500 via-green-500 to-green-300">
+      <div className="text-center py-5 px-5">
+        <h2 className="text-2xl lg:text-4xl font-semibold  text-white">
+          Desarrolladores de la{" "}
+          <span className="text-blue-600">iniciativa</span>
+        </h2>
+      </div>
+      <div className="flex flex-col justify-between p-3 lg:p-5 border border-gray-200/40 bg-white/20  w-[70%] min-w-80 rounded-3xl shadow-2xl backdrop-blur-2xl">
+        <Image className="w-[100%]  rounded-2xl " src={ImagenGrupo} alt="" />
+      </div>
+    </section>
+  );
+}
