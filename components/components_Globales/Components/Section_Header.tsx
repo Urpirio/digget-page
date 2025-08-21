850ef6aa56cd4fd1b1d82cbfd924c8d2c4c4e272
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-20 22:03:35 -0400

    Se hicieron mejoras al componente, esto para la responsividad

--- components/components_Globales/Components/Section_Header.tsx
+++ components/components_Globales/Components/Section_Header.tsx
@@ -4,18 +4,20 @@ import { CiSaveDown2 } from "react-icons/ci";
 import LogoPage from "@/../public/img/Group 4.svg";
 import Image from "next/image";
 import { motion } from "motion/react";
+import { RiMenu4Fill } from "react-icons/ri";
 
 export default function Section_Header() {
+  
   return (
     <header className="w-full py-10 fixed  justify-center flex  ">
       <motion.section
         animate={{ translateY: 120 }}
-        className="flex -translate-y-30 w-[85%] rounded-2xl  items-center b bg-white/80  backdrop-blur-2xl border border-gray-300/40  py-4 px-5  "
+        className="flex -translate-y-30 w-[85%] rounded-2xl  items-center b bg-white/80  backdrop-blur-2xl border border-gray-300/40  py-5 px-5  "
       >
         <div className="flex items-center w-1/4  flex-grow">
           <Image className="h-10 w-10" src={LogoPage} alt="" />
         </div>
-        <nav className="flex  gap-5 w-1/2 text-sm justify-center">
+        <nav className=" md:flex hidden  gap-5 w-1/2 text-sm justify-center">
           <Link
             href={""}
             className="text-gray-700  hover:text-blue-500 transition-colors duration-300"
@@ -41,10 +43,15 @@ export default function Section_Header() {
             Contacto
           </Link>
         </nav>
-        <div className="flex items-center w-1/4 flex-grow justify-end gap-4">
+        <div className="md:flex hidden items-center w-1/4 flex-grow justify-end gap-4">
           <button className="bg-blue-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 cursor-pointer hover:opacity-80 transition-opacity duration-300">
             <CiSaveDown2 className="text-xl" />
-            <span>Descargar App</span>
+            <span className="text-xs lg:text-lg">Descargar App</span>
+          </button>
+        </div>
+        <div className="md:hidden flex items-center w-1/4 flex-grow justify-end gap-4">
+          <button className="bg-blue-600 text-white p-3 rounded-lg flex items-center gap-2 cursor-pointer hover:opacity-80 transition-opacity duration-300">
+            <RiMenu4Fill  className="text-white text-2xl"/>
           </button>
         </div>
       </motion.section>
