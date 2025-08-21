a0f696ed17eed130cabc777cf6ef22fb382f1992
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-21 08:29:39 -0400

    Se agrego la primera fase del footer

--- components/components_Globales/Components/Section_Footer.tsx
+++ components/components_Globales/Components/Section_Footer.tsx
@@ -1,9 +1,12 @@
-
-
 export default function Section_Footer() {
   return (
-   <footer>
-    
-   </footer>
-  )
+    <footer>
+      <section></section>
+      <section className="py-5 bg-gray-900">
+        <div className="text-center">
+          <h5 className="text-white font-light" >© 2025 DIGGE-App. <br className="lg:hidden"/> Todos los derechos reservados.</h5>
+        </div>
+      </section>
+    </footer>
+  );
 }
