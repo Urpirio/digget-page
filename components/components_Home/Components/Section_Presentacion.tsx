7e8d246f77393ebd8d38887fafd7cd8375e7edb2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-21 10:11:45 -0400

    Se hicieron mejoras en la seccion y se agrego la funcion para descargar el contenido

--- components/components_Home/Components/Section_Presentacion.tsx
+++ components/components_Home/Components/Section_Presentacion.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import AppReferencia from "@/../public/img/Group 208.svg";
 import { CiSaveDown2 } from "react-icons/ci";
+import Link from "next/link";
 
 export default function Section_Presentacion() {
   return (
-    <section className="flex  flex-wrap  items-center justify-center pb-10 pt-38 md:pt-30 px-5 lg:px-30 gap-5 md:gap-0  bg-white">
+    <section className="flex  flex-wrap  items-center justify-center pb-10 pt-38 md:pt-30 px-5 lg:px-30 gap-5 md:gap-0  bg-white" id="presentacion">
       <div className="flex flex-col gap-4  w-1/2 min-w-80">
         <h2 className="xl:text-7xl text-4xl font-bold">
           DIGGE-App 2025:{" "}
@@ -16,11 +17,15 @@ export default function Section_Presentacion() {
           Dominicana. Gestiona tus vehículos, reporta incidencias y paga
           infracciones de manera transparente.
         </p>
-        <div>
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 cursor-pointer hover:opacity-80 transition-opacity duration-300">
+        <div className="flex justify-start">
+          <Link
+            href={"pdf/Presentacion.pdf"}
+            download={"PresentacionDigge-app.pdf"}
+            className="bg-blue-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 cursor-pointer hover:opacity-80 transition-opacity duration-300"
+          >
             <CiSaveDown2 className="text-xl" />
-            <span>Descargar App</span>
-          </button>
+            <span>Descargar presentacion</span>
+          </Link>
         </div>
       </div>
       <div className=" w-1/2 min-w-80 flex md:justify-end  justify-center ">
