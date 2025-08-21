4a90c35f67ce437ffa281d9fd9bd695d19872f9e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-20 22:05:20 -0400

    Se agregaron dos nuevas secciones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import Section_BeneficiosCiudadano from "../../components/components_Home/Components/Section_BeneficiosCiudadano";
 import Section_Funcionalidades from "../../components/components_Home/Components/Section_Funcionalidades";
+import Section_Funcionalidades2 from "../../components/components_Home/Components/Section_Funcionalidades2";
 import Section_Presentacion from "../../components/components_Home/Components/Section_Presentacion";
 
 export default function page() {
@@ -6,6 +8,8 @@ export default function page() {
     <main>
       <Section_Presentacion/>
       <Section_Funcionalidades/>
+      <Section_Funcionalidades2/>
+      <Section_BeneficiosCiudadano/>
     </main>
   )
 }
