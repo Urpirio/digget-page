e7ec598f8c4cadc6821dde003f08d22cd20c237c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-21 07:01:04 -0400

    Se hicieron mejoras en la page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
+"use client";
 import Section_BeneficiosCiudadano from "../../components/components_Home/Components/Section_BeneficiosCiudadano";
 import Section_Funcionalidades from "../../components/components_Home/Components/Section_Funcionalidades";
 import Section_Funcionalidades2 from "../../components/components_Home/Components/Section_Funcionalidades2";
 import Section_Presentacion from "../../components/components_Home/Components/Section_Presentacion";
+import Section_Header from "../../components/components_Globales/Components/Section_Header";
+import Section_Footer from "../../components/components_Globales/Components/Section_Footer";
+import { Albert_Sans } from "next/font/google";
+import { useHeader } from "../../components/components_Home/Hooks/useHeader";
 
-export default function page() {
+const albert_Sans = Albert_Sans({
+  subsets: ["latin-ext"],
+});
+
+
+export default function Home() {
+  const { StatusMenu, setStatusMenu } = useHeader();
   return (
-    <main>
-      <Section_Presentacion/>
-      <Section_Funcionalidades/>
-      <Section_Funcionalidades2/>
-      <Section_BeneficiosCiudadano/>
-    </main>
-  )
+    <body
+      onTouchMove={() => setStatusMenu(false)}
+      className={`${albert_Sans.className}`}
+    >
+      <Section_Header StatusMenu={StatusMenu} setStatusMenu={setStatusMenu} />
+      <main>
+        <Section_Presentacion />
+        <Section_Funcionalidades />
+        <Section_Funcionalidades2 />
+        <Section_BeneficiosCiudadano />
+      </main>
+      <Section_Footer />
+    </body>
+  );
 }
