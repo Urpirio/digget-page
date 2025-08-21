6d40e1e657e1b44edfd10efc6cf6be2e01b725e8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-21 10:10:55 -0400

    Se hicieron mejoras de estilos

--- components/components_Home/Components/Section_Grupo.tsx
+++ components/components_Home/Components/Section_Grupo.tsx
@@ -3,7 +3,7 @@ import ImagenGrupo from "@/../public/img/ImagenGrupo.svg";
 
 export default function Section_Grupo() {
   return (
-    <section className="py-10 flex  flex-col justify-center items-center bg-linear-to-tl from-blue-500 via-green-500 to-green-300">
+    <section className="py-10 flex  flex-col justify-center items-center bg-linear-to-tl from-blue-500 via-green-500 to-green-300" id="Equipo">
       <div className="text-center py-5 px-5">
         <h2 className="text-2xl lg:text-4xl font-semibold  text-white">
           Desarrolladores de la{" "}
