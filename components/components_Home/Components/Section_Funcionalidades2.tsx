86ffc2c57243c4532124cc0491a7888a6a4519d5
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-20 22:04:11 -0400

    Se hicieron mejoras al componente, esto para la responsividad

--- components/components_Home/Components/Section_Funcionalidades2.tsx
+++ components/components_Home/Components/Section_Funcionalidades2.tsx
@@ -1,11 +1,14 @@
-
 export default function Section_Funcionalidades2() {
   return (
-    <section>
-        <div>
-            <h3></h3>
-            <p></p>
-        </div>
+    <section className=" w-full flex justify-center items-center py-5 px-10">
+      <div className="border bg-linear-to-l w-[60%] min-w-80 from-blue-500 to-green-500 text-white p-10 rounded-2xl text-center flex flex-col gap-2">
+        <h3 className="text-2xl">Todo en una sola aplicación</h3>
+        <p className="text-md font-light">
+          DIGGE-App integra todos los servicios viales que necesitas en una
+          plataforma moderna, segura y fácil de usar. La digitalización del
+          futuro, disponible hoy.
+        </p>
+      </div>
     </section>
-  )
+  );
 }
